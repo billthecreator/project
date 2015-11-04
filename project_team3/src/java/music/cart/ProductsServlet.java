d0e7edd5feaad2edda10a2734b43e7bd34a345e3
William <billreithmeyer@gmail.com>
2015-11-03 19:34:22 -0500

    Project 1 v 8
    code comments

--- project_team3/src/java/music/cart/ProductsServlet.java
+++ project_team3/src/java/music/cart/ProductsServlet.java
@@ -17,34 +17,45 @@ public class ProductsServlet extends HttpServlet {
 
         HttpSession session = request.getSession();
         
+        //get the path of the file containing the products
         String path = getServletContext().getRealPath("/WEB-INF/products.txt");
+        //create a product list holder
         ProductIO prodIO = new ProductIO();
         prodIO.init(path);
-        
+        //put the list of products in a List
         List<Product> products = prodIO.selectProducts();
+        //set the List to the "products" attribute
         session.setAttribute("products", products);
         
-        // create the action 
+        //default landing page
+        String url = "/productMaint.jsp";
+        //create the action 
         String action = request.getParameter("action");
         if (action == null) {
-            action = "displayProducts";  // default action
+            //default action
+            action = "displayProducts"; 
         }
-
-        String url = "/productMaint.jsp";
+        
+        //actions:
         if (action.equals("displayProducts")) {
+            //default action, just show the list of products
             url = "/productMaint.jsp";    
         } 
         else if (action.equals("addProduct")) {
+            //adding or editing a product
             String productCode = request.getParameter("productCode");
             
             url = "/addProduct.jsp";
             if (productCode != null) {
-                
+                //if the product code not not a null value,
+                //grab the details from the product code
                 session.setAttribute("productCode", productCode);
                 session.setAttribute("productDesc", prodIO.selectProduct(productCode).getDescription());
                 session.setAttribute("productPrice", prodIO.selectProduct(productCode).getPrice());
             }
             else {
+                //if the product code is null, set everything blank
+                //so the the product is new
                 session.setAttribute("productCode", "");
                 session.setAttribute("productDesc", "");
                 session.setAttribute("productPrice", "");
@@ -52,10 +63,12 @@ public class ProductsServlet extends HttpServlet {
             
         }
         else if (action.equals("updateProduct")) {
+            //create or update the product based on the product code.
             String productCode = request.getParameter("productCode");
             String productDesc = request.getParameter("productDesc");
             String productPrice= request.getParameter("productPrice");
             
+            //if the product code has any value 
             if (productCode != null || !productCode.isEmpty()){
                 Product newProduct = new Product();
                 newProduct.setCode(productCode);
@@ -70,18 +83,20 @@ public class ProductsServlet extends HttpServlet {
                     prodIO.insertProduct(newProduct);
                 }
                 url = "/productMaint.jsp?action=displayProducts";
+                //#####
+                //change this to make it work. manual refresh at the moment
             } else {
                 url = "/addProduct.jsp";
             }
             
         }
         else if (action.equals("removeProduct")) {
+            //remove the product from the list
             String productCode = request.getParameter("productCode");
             
             if (productCode != null || !productCode.isEmpty()){
                 Product productToDelete = prodIO.selectProduct(productCode);
-                
-                
+                                
                 if (prodIO.exists(productToDelete.getCode())){
                     // if this product exists in the list, then update it
                     prodIO.deleteProduct(productToDelete);
